ede45ad4630241eed04f60a5bb3b9593c77d4ea9
Oscar Viciana <oscar.viciana@atrapalo.com>
2019-03-27 12:21:50 +0100

    refactor(filter): update the filter to use reducer function.
    affects: @functional/filter

--- packages/filter/src/index.ts
+++ packages/filter/src/index.ts
@@ -1,14 +1,7 @@
-import forEach from '@functional/for-each';
+import reduce from '@functional/reduce';
 
 function filter<T>(action: (item: T) => boolean, list: T[]): T[] {
-  const newList: T[] = [];
-  forEach((item) => {
-    if (action(item)) {
-      newList.push(item);
-    }
-  }, list);
-
-  return newList;
+  return reduce((acc, curr) => (action(curr) ? [...acc, curr] : acc), [], list);
 }
 
 export default filter;
