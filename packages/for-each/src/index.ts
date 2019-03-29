ecb2fac5194efcbe4a93e260f47326be943cfdb6
Oscar Viciana <oscar.viciana@atrapalo.com>
2019-03-29 10:25:46 +0100

    fix(for-each): when don't pass items, should exit of the function.
    affects: @functional/filter, @functional/for-each, @functional/map, @functional/reduce

--- packages/for-each/src/index.ts
+++ packages/for-each/src/index.ts
@@ -1,8 +1,13 @@
 function forEach<T>(action: (item: T) => void, list: T[]) {
+  if (list.length <= 0) {
+    return;
+  }
+
   action(list[0]);
   if (list.length <= 1) {
     return;
   }
+
   forEach(action, list.slice(1, list.length));
 }
 
